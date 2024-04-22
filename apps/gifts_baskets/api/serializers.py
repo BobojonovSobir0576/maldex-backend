42d55a956b0d540dc2ed5c53ac8dd9a484a6aa7d
BobojonovSobir0576 <dweb0987654321@gmail.com>
2024-04-22 09:54:14 +0500

    changed something

--- apps/gifts_baskets/api/serializers.py
+++ apps/gifts_baskets/api/serializers.py
@@ -84,8 +84,11 @@ class GiftBasketListSerializers(serializers.ModelSerializer):
 
     def create(self, validated_data):
         categories = validated_data.pop('category_data', [])
+        print(categories)
         images = validated_data.pop('images_data', [])
+        print(images)
         products = validated_data.pop('products_data', [])
+        print(products)
         basket = GiftsBaskets.objects.create(**validated_data)
         basket.gift_basket_category.set(categories)
 
--- apps/gifts_baskets/api/views/admin_file.py
+++ apps/gifts_baskets/api/views/admin_file.py
@@ -24,7 +24,7 @@ class AdminFilesListView(APIView):
                          tags=['Admin files list'],
                          responses={200: AdminFilesListSerializer(many=True)})
     def get(self, request):
-        queryset = AdminFiles.objects.all().order_by('-id')
+        queryset = AdminFiles.objects.all()
         serializer = AdminFilesListSerializer(queryset, many=True, context={'request': request})
         return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -33,7 +33,7 @@ class AdminFilesListView(APIView):
                          tags=['Admin files list'],
                          responses={201: AdminFilesListSerializer(many=False)})
     def post(self, request):
-        valid_fields = {'name', 'files',}
+        valid_fields = {'name', 'files'}
         unexpected_fields = check_required_key(request, valid_fields)
         if unexpected_fields:
             return bad_request_response(f"Unexpected fields: {', '.join(unexpected_fields)}")
@@ -63,7 +63,7 @@ class AdminFilesDetailView(APIView):
                          tags=['Admin files list'],
                          responses={200: AdminFilesListSerializer(many=False)})
     def put(self, request, pk):
-        valid_fields = {'name', 'files',}
+        valid_fields = {'name', 'files'}
         unexpected_fields = check_required_key(request, valid_fields)
         if unexpected_fields:
             return bad_request_response(f"Unexpected fields: {', '.join(unexpected_fields)}")
