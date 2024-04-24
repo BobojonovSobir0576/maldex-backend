1e6e8b2fc484f4d0b9ec1c5eaa86ab620af015b1
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-04-24 15:47:40 +0500

    added tags

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
 
-from apps.blog.models import Article, Project, FAQ, PrintCategory
+from apps.blog.models import Article, Project, FAQ, PrintCategory, Tag
 
 
 class FAQAdmin(admin.ModelAdmin):
@@ -10,7 +11,28 @@ class FAQAdmin(admin.ModelAdmin):
     list_filter = ['type']
 
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['title', 'body']
+    fields = ['title', 'body', 'tags', 'image']
+    list_filter = ['tags']
+    filter_horizontal = ['tags']
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'content_type']
+    search_fields = ['name']
+    fields = ['name', 'content_type']
+    list_filter = ['content_type']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'content_type':
+            kwargs['queryset'] = ContentType.objects.filter(app_label='blog')
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+admin.site.register(Article, ArticleAdmin)
 admin.site.register(Project)
 admin.site.register(FAQ, FAQAdmin)
 admin.site.register(PrintCategory)
+admin.site.register(Tag, TagAdmin)
--- apps/blog/serializers.py
+++ apps/blog/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from taggit.models import TaggedItem
 
-from apps.blog.models import Article, Project, FAQ, PrintCategory
+from apps.blog.models import Article, Project, FAQ, PrintCategory, Tag
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
 
 
 class ArticleSerializer(serializers.ModelSerializer):
@@ -12,8 +17,7 @@ class ArticleSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'body', 'tags', 'image', 'pub_date']
 
     def get_tags(self, obj):
-        print([tag.name for tag in obj.tags.all()])
-        return [tag.name for tag in obj.tags.all()]
+        return [tag['name'] for tag in obj.tags.all().values('name')]
 
 
 class ProjectSerializer(serializers.ModelSerializer):
