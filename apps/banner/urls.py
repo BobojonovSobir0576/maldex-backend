b3dcf4ddf49ab7f0049f28c26ae50e9c9f0a9c73
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-05-02 10:43:12 +0500

    uuid

--- apps/banner/urls.py
+++ apps/banner/urls.py
@@ -5,7 +5,7 @@ from apps.banner.api.views import banner
 urlpatterns = [
     path('', banner.BannerListView.as_view()),
     path('<uuid:pk>/', banner.BannerDetailView.as_view()),
-    path('product/<int:pk>/', banner.BannerProductDetailView.as_view()),
+    path('product/<uuid:pk>/', banner.BannerProductDetailView.as_view()),
     path('carousel/', banner.BannerCarouselListView.as_view()),
     path('carousel/<int:pk>/', banner.BannerCarouselDetailView.as_view()),
     path('carousel/product/<int:pk>/', banner.BannerCarouselProductDetailView.as_view()),
