f53c5649e8b48b015b4cc9e13d0da052a34a5412
Komronbek Obloev <89721936+ok7uz@users.noreply.github.com>
2024-05-11 11:08:54 +0500

    date to date time

--- apps/product/api/serializers.py
+++ apps/product/api/serializers.py
@@ -350,7 +350,7 @@ class ProductAutoUploaderSerializer(serializers.ModelSerializer):
             else:
                 image_url = img['name']
                 response = requests.get(image_url)
-                name = f'midea/gifts/{uuid.uuid4()}.jpg'
+                name = f'midea/product/{uuid.uuid4()}.jpg'
                 file = open(name, 'wb')
                 file.write(response.content)
                 file.close()
