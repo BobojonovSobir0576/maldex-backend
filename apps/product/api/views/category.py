4acb1b75845533bce57dc4502e0edc084ed9deb4
ok7uz <komronbekobloev@gmail.com>
2024-05-20 16:07:49 +0500

    ..

--- apps/product/api/views/category.py
+++ apps/product/api/views/category.py
@@ -39,7 +39,7 @@ class CategoryListView(APIView):
         filterset = ProductCategoryFilter(request.GET, queryset=queryset)
         if filterset.is_valid():
             queryset = filterset.qs
-        if all(filter(lambda p: p.is_popular, queryset)):
+        if request.query_params.get('is_popular', None) is True:
             queryset = queryset.order_by('order_top')
         serializers = MainCategorySerializer(queryset, many=True, context={'request': request})
         return success_response(serializers.data)
